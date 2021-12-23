d161049289c7c6854951d299f9be8bb7d61dad8e
simpart <taria.ki4m@gmail.com>
2021-12-23 13:52:17 +0000

    first

---
+++ README.md
@@ -0,0 +1,25 @@
+# mofron-comp-jspreadsheet
+[mofron](https://mofron.github.io/mofron/) is module based frontend framework.
+
+jspreadsheet component for mofron
+
+
+# Install
+```
+npm install mofron mofron-comp-jspreadsheet
+```
+
+# Sample
+```html
+<setting>
+    <tag load="mofron-comp-errdialog">ErrDialog</tag>
+</setting>
+
+<ErrDialog>Error Dialog</ErrDialog>
+```
+
+# Parameter
+
+| Short<br>Form | Parameter Name | Type | Description |
+|:-------------:|:---------------|:-----|:------------|
+
---
+++ index.js
@@ -0,0 +1,83 @@
+/**
+ * @file mofron-comp-jspreadsheet/index.js
+ * @brief jspreadsheet component for mofron
+ * @license MIT
+ */
+const Dialog = require('mofron-comp-dialog'); 
+const Text   = require('mofron-comp-text');
+const HrzPos = require('mofron-effect-hrzpos');
+
+module.exports = class extends Dialog {
+    /**
+     * initialize component
+     * 
+     * @param (mixed) string: label text
+     *                key-value: component config
+     * @type private
+     */
+    constructor (p1) {
+        try {
+            super();
+            this.modname("ErrDialog");
+            this.shortForm("text");
+	    
+            /* init config */
+	    if (0 < arguments.length) {
+                this.config(p1);
+            }
+        } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+    
+    /**
+     * initialize dom contents
+     * 
+     * @type private
+     */
+    initDomConts () {
+        try {
+            super.initDomConts();
+            this.size("3.5rem","2rem");
+            this.color([255,230,230]);
+            this.title("error");
+            this.button("OK", {
+                "clickEvent" : new mofron.class.ConfArg(
+		    (b1,b2,b3) => { b3.visible(false); },
+		    this
+		)
+	    });
+            
+	    this.text().config({
+	        style: { "margin-top" : "0.4rem" },
+                size:  "0.2rem",
+		effect: new HrzPos("center")
+            });
+	    this.child(this.text());
+        } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+    
+    /**
+     * error text
+     * 
+     * @
+     */
+    text (prm, opt) {
+        try {
+            if ("string" === typeof prm) {
+                this.text().text(prm);
+                this.text().config(opt);
+                return;
+            }
+            return this.innerComp("text", prm, Text);
+	} catch (e) {
+            console.error(e.stack);
+            throw e;
+	}
+    }
+}
+/* end of file */
